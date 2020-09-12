f09989269945c57de233e6434b59cc2c2aa6db13
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-12 16:55:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
 
 
   def apply_discount
-    @total -= (@total * @discount.to_f / 100).to_f
+    @total -= (@total * @discount.to_f / 100).to_i
     if @discount == 0
       "There is no discount to apply."
     else
