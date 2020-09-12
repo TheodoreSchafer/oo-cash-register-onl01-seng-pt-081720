1649a8fb46acdce5744e74633ae22b0e5c0e8568
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-12 16:13:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,7 +6,9 @@ class CashRegister
     @discount = discount
   end
 
-  def add_item(title, price)
+  def add_item(title, price, quantity)
+    self.total = price * quantity
+    @items << title
 
 
   end
