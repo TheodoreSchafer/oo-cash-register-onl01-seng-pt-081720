3154452f533a1b8bcae9085a481cbb24f8d3e34e
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-12 16:22:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :total, :discount, :items
+  attr_accessor :total, :discount, :items, 
 
   def initialize(discount = 0)
     @total = 0
@@ -17,7 +17,7 @@ class CashRegister
   end
 
   def void_last_transaction
-  end 
+  end
 
 
 
